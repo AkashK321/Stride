9373f891857d6d000841cf6f528b1da76f0f9842
Rory Tippery <rory.tippery1@outlook.com>
2026-01-27 12:17:40 -0500

    added default exports

---
+++ frontend/app/(tabs)/settings.tsx
@@ -0,0 +1,17 @@
+import * as React from "react";
+import { View, Text } from "react-native";
+
+export default function Settings() {
+  return React.createElement(
+    View,
+    {
+      style: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      },
+    },
+    React.createElement(Text, null, "Settings"),
+  );
+}
+
