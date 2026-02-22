f5a0f32fc20beb3f3ec9ee543df2b38c3ebe59b3
Hilal B Tasdemir <hilalbtasdemir@gmail.com>
2026-02-22 15:35:53 -0500

    ec2 import fixed

--- aws_resources/cdk/cdk_stack.py
+++ aws_resources/cdk/cdk_stack.py
@@ -11,9 +11,12 @@ from aws_cdk import (
     aws_apigateway as apigw,
     aws_apigatewayv2 as apigw_v2,
     aws_apigatewayv2_integrations as integrations,
+    aws_ec2 as ec2,  
+    aws_rds as rds,  
     aws_dynamodb as ddb,
     RemovalPolicy,
     aws_cognito as cognito,
+    custom_resources as cr,
     BundlingOptions,
     aws_iam as iam,
     CustomResource,
@@ -27,7 +30,7 @@ class CdkStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
-        # default_vpc = ec2.Vpc.from_lookup(self, "DefaultVPC", is_default=True)
+        default_vpc = ec2.Vpc.from_lookup(self, "DefaultVPC", is_default=True)
 
         # Path to the Kotlin backend project
         this_dir = os.path.dirname(__file__)
@@ -320,3 +323,60 @@ class CdkStack(Stack):
             value=user_pool_client.user_pool_client_id,
             description="Cognito User Pool Client ID"
         )
+
+        # TODO: RDS setup disabled for now - to be re-enabled when ready
+        # Define RDS Resource
+        db_instance = rds.DatabaseInstance(
+            self, "StrideDB",
+            engine=rds.DatabaseInstanceEngine.postgres(
+                version=rds.PostgresEngineVersion.VER_16_3
+            ),
+            vpc=default_vpc,  # Mandatory, but now using the free default one
+            vpc_subnets=ec2.SubnetSelection(subnet_type=ec2.SubnetType.PUBLIC),
+            instance_type=ec2.InstanceType.of(
+                ec2.InstanceClass.BURSTABLE3, ec2.InstanceSize.MICRO
+            ),
+            allocated_storage=20,
+            max_allocated_storage=50, # Autoscaling storage
+            database_name="StrideCore",
+            publicly_accessible=True, # Allows Lambda to connect via standard internet
+            removal_policy=RemovalPolicy.DESTROY, # For dev/testing only
+        )
+
+        db_instance.connections.allow_from_any_ipv4(ec2.Port.tcp(5432), "Allow public access for Lambda")
+
+        # Define the lambda to initialize the DB schema
+        schema_lambda = _lambda.Function(
+            self, "SchemaInitializer",
+            runtime=_lambda.Runtime.PYTHON_3_9,
+            handler="populate_rds.handler",
+            code=_lambda.Code.from_asset("schema_initializer"),
+            timeout=Duration.seconds(30),
+            environment={
+                # Retrieve connection details from the secret
+                "DB_SECRET_ARN": db_instance.secret.secret_arn
+            }
+        )
+        db_instance.secret.grant_read(schema_lambda)
+
+        # Trigger the schema initialization during deployment
+        invoke_schema_lambda = cr.AwsSdkCall(
+            service="Lambda",
+            action="invoke",
+            parameters={
+                "FunctionName": schema_lambda.function_name
+            },
+            physical_resource_id=cr.PhysicalResourceId.of("SchemaInit_Update")
+        )
+        cr.AwsCustomResource(
+            self, "InitDBSchema",
+            on_create=invoke_schema_lambda,
+            on_update=invoke_schema_lambda,
+            policy=cr.AwsCustomResourcePolicy.from_statements([
+                iam.PolicyStatement(
+                    actions=["lambda:InvokeFunction"],
+                    resources=[schema_lambda.function_arn]
+                )
+            ])
+        )
+
