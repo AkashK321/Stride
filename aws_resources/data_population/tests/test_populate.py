8397a984df3f5b2b752509026f91497ca490ade9
Hilal B Tasdemir <hilalbtasdemir@gmail.com>
2026-02-22 15:59:53 -0500

    test imports fixed

--- aws_resources/data_population/tests/test_populate.py
+++ aws_resources/data_population/tests/test_populate.py
@@ -1,5 +1,9 @@
 import pytest
 import math
+import sys
+import os
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from populate_floor_data import (
     calculate_bearing,
     calculate_distance,
