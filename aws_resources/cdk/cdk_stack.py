c1822fa76c586802895166ffe891ac095175975a
Hilal B Tasdemir <hilalbtasdemir@gmail.com>
2026-02-22 15:29:31 -0500

    default vpc taken down

--- aws_resources/cdk/cdk_stack.py
+++ aws_resources/cdk/cdk_stack.py
@@ -30,7 +30,7 @@ class CdkStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
-        default_vpc = ec2.Vpc.from_lookup(self, "DefaultVPC", is_default=True)
+        # default_vpc = ec2.Vpc.from_lookup(self, "DefaultVPC", is_default=True)
 
         # Path to the Kotlin backend project
         this_dir = os.path.dirname(__file__)
