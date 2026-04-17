3da61e7dff948c07e582b55b0f1ecf95a909466e
Hilal B Tasdemir <hilalbtasdemir@gmail.com>
2026-04-16 23:39:42 -0400

    floor2 added - landmarks tbd

--- aws_resources/map_population/floor_data/floor0/nodes.py
+++ aws_resources/map_population/floor_data/floor0/nodes.py
@@ -22,8 +22,8 @@ NODES = [
                 id="room_016",
                 label="Room 016",
                 side_by_bearing=[
-                    DoorSideByBearing(bearing_deg=180, side="right"), 
-                    DoorSideByBearing(bearing_deg=0, side="left"),
+                    DoorSideByBearing(bearing_deg=0, side="right"),  # 51 degrees
+                    DoorSideByBearing(bearing_deg=180, side="left"),  
                 ],
             ),
         ],
@@ -49,8 +49,8 @@ NODES = [
                 # 042 is straight from this node, no turn is needed.
 
                 side_by_bearing=[
-                    DoorSideByBearing(bearing_deg=270, side="right"), 
-                    DoorSideByBearing(bearing_deg=90, side="left"), 
+                    DoorSideByBearing(bearing_deg=0, side="right"), # 51 degrees
+                    DoorSideByBearing(bearing_deg=180, side="left"), 
                 ],
             ),
         ],
@@ -197,8 +197,8 @@ NODES = [
                 # 042 is straight from this node, no turn is needed.
 
                 side_by_bearing=[
-                    DoorSideByBearing(bearing_deg=270, side="right"), 
-                    DoorSideByBearing(bearing_deg=90, side="left"), 
+                    DoorSideByBearing(bearing_deg=0, side="right"), 
+                    DoorSideByBearing(bearing_deg=180, side="left"), 
                 ],
             ),
         ],
@@ -212,8 +212,8 @@ NODES = [
                 id="room_044",
                 label="Room 044",
                 side_by_bearing=[
-                    DoorSideByBearing(bearing_deg=0, side="right"), 
-                    DoorSideByBearing(bearing_deg=180, side="left"), 
+                    DoorSideByBearing(bearing_deg=180, side="right"), 
+                    DoorSideByBearing(bearing_deg=0, side="left"), 
                 ],
             ),
         ],
@@ -227,8 +227,8 @@ NODES = [
                 id="room_046",
                 label="Room 046",
                 side_by_bearing=[
-                    DoorSideByBearing(bearing_deg=0, side="right"), 
-                    DoorSideByBearing(bearing_deg=180, side="left"), 
+                    DoorSideByBearing(bearing_deg=180, side="right"), 
+                    DoorSideByBearing(bearing_deg=0, side="left"), 
                 ],
             ),
             DoorRef(
@@ -250,8 +250,8 @@ NODES = [
                 id="room_048",
                 label="Room 048",
                 side_by_bearing=[
-                    DoorSideByBearing(bearing_deg=0, side="right"), 
-                    DoorSideByBearing(bearing_deg=180, side="left"), 
+                    DoorSideByBearing(bearing_deg=180, side="right"), 
+                    DoorSideByBearing(bearing_deg=0, side="left"), 
                 ],
             ),
         ],
@@ -265,8 +265,8 @@ NODES = [
                 id="room_050",
                 label="Room 050",
                 side_by_bearing=[
-                    DoorSideByBearing(bearing_deg=0, side="right"), 
-                    DoorSideByBearing(bearing_deg=180, side="left"), 
+                    DoorSideByBearing(bearing_deg=180, side="right"), 
+                    DoorSideByBearing(bearing_deg=0, side="left"), 
                 ],
             ),
         ],
@@ -280,8 +280,8 @@ NODES = [
                 id="room_052",
                 label="Room 052",
                 side_by_bearing=[
-                    DoorSideByBearing(bearing_deg=0, side="right"), 
-                    DoorSideByBearing(bearing_deg=180, side="left"), 
+                    DoorSideByBearing(bearing_deg=180, side="right"), 
+                    DoorSideByBearing(bearing_deg=0, side="left"), 
                 ],
             ),
         ],
@@ -295,8 +295,8 @@ NODES = [
                 id="room_054",
                 label="Room 054",
                 side_by_bearing=[
-                    DoorSideByBearing(bearing_deg=0, side="right"), 
-                    DoorSideByBearing(bearing_deg=180, side="left"), 
+                    DoorSideByBearing(bearing_deg=180, side="right"), 
+                    DoorSideByBearing(bearing_deg=0, side="left"), 
                 ],
             ),
         ],
@@ -310,8 +310,8 @@ NODES = [
                 id="room_054",
                 label="Room 054B",
                 side_by_bearing=[
-                    DoorSideByBearing(bearing_deg=0, side="right"), 
-                    DoorSideByBearing(bearing_deg=180, side="left"), 
+                    DoorSideByBearing(bearing_deg=180, side="right"), 
+                    DoorSideByBearing(bearing_deg=0, side="left"), 
                 ],
             ),
         ],
@@ -325,8 +325,8 @@ NODES = [
                 id="room_056",
                 label="Room 056",
                 side_by_bearing=[
-                    DoorSideByBearing(bearing_deg=0, side="right"), 
-                    DoorSideByBearing(bearing_deg=180, side="left"), 
+                    DoorSideByBearing(bearing_deg=180, side="right"), 
+                    DoorSideByBearing(bearing_deg=0, side="left"), 
                 ],
             ),
         ],
@@ -340,8 +340,8 @@ NODES = [
                 id="room_056b",
                 label="Room 056b",
                 side_by_bearing=[
-                    DoorSideByBearing(bearing_deg=0, side="right"), 
-                    DoorSideByBearing(bearing_deg=180, side="left"), 
+                    DoorSideByBearing(bearing_deg=180, side="right"), 
+                    DoorSideByBearing(bearing_deg=0, side="left"), 
                 ],
             ),
         ],
@@ -403,8 +403,8 @@ NODES = [
                 id="room_059",
                 label="Room 059",
                 side_by_bearing=[
-                    DoorSideByBearing(bearing_deg=270, side="left"), # 321 degrees
-                    DoorSideByBearing(bearing_deg=90, side="right"), # 141 degrees 
+                    DoorSideByBearing(bearing_deg=180, side="left"), # 321 degrees
+                    DoorSideByBearing(bearing_deg=0, side="right"), # 141 degrees 
                 ],
             ),
         ],
@@ -465,14 +465,14 @@ NODES = [
                 id="room_007",
                 label="Room 007",
                 side_by_bearing=[
-                    DoorSideByBearing(bearing_deg=180, side="right"), 
-                    DoorSideByBearing(bearing_deg=0, side="left"), 
+                    DoorSideByBearing(bearing_deg=270, side="left"), 
+                    DoorSideByBearing(bearing_deg=90, side="right"),
                 ],
             ),
         ],
     ).asdict(),
     HallwayPointNode(
-        id="hall005",
+        id="hall_005",
         x_feet= -70,
         y_feet= 65,
         doors=[
@@ -480,16 +480,16 @@ NODES = [
                 id="room_005b",
                 label="Room 005B",
                 side_by_bearing=[
-                    DoorSideByBearing(bearing_deg=0, side="right"), 
-                    DoorSideByBearing(bearing_deg=180, side="left"), 
+                    DoorSideByBearing(bearing_deg=0, side="left"), 
+                    DoorSideByBearing(bearing_deg=180, side="right"), 
                 ],
             ),
             DoorRef(
                 id="room_005",
                 label="Room 005",
                 side_by_bearing=[
-                    DoorSideByBearing(bearing_deg=0, side="right"), 
-                    DoorSideByBearing(bearing_deg=180, side="left"), 
+                    DoorSideByBearing(bearing_deg=0, side="left"), 
+                    DoorSideByBearing(bearing_deg=180, side="right"), 
                 ],
             ),
         ],
@@ -504,8 +504,8 @@ NODES = [
                 id="room_011",
                 label="Room 011",
                 side_by_bearing=[
-                    DoorSideByBearing(bearing_deg=0, side="right"), 
-                    DoorSideByBearing(bearing_deg=180, side="left"), 
+                    DoorSideByBearing(bearing_deg=0, side="left"), 
+                    DoorSideByBearing(bearing_deg=180, side="right"), 
                 ],
             ),
         ],
@@ -519,8 +519,8 @@ NODES = [
                 id="room_013",
                 label="Room 013",
                 side_by_bearing=[
-                    DoorSideByBearing(bearing_deg=0, side="right"), 
-                    DoorSideByBearing(bearing_deg=180, side="left"), 
+                    DoorSideByBearing(bearing_deg=0, side="left"), 
+                    DoorSideByBearing(bearing_deg=180, side="right"), 
                 ],
             ),
         ],
@@ -586,7 +586,96 @@ NODES = [
         ],
     ).asdict(),
 
+    HallwayPointNode(
+        id="hall_129",
+        x_feet= -41,
+        y_feet= 93,
+        doors=[
+            DoorRef(
+                id="room_129low",
+                label="Room 129",
+                side_by_bearing=[
+                    DoorSideByBearing(bearing_deg=0, side="left"), 
+                    DoorSideByBearing(bearing_deg=180, side="right"), 
+                ],
+            ),
+        ],
+    ).asdict(),
 
+    # corner nodes
+    HallwayPointNode(
+        id="corner_southwest",
+        x_feet=-83,
+        y_feet=4.5,
+        # No doors, just a corner
+    ).asdict(),
+    
+    HallwayPointNode(
+        id="corner_southeast",
+        x_feet=82.5,
+        y_feet=4.5,
+    ).asdict(),
+
+    HallwayPointNode(
+        id="corner_northeast",
+        x_feet=87,
+        y_feet=103,
+    ).asdict(),
+
+    HallwayPointNode(
+        id="corner_rm005",
+        x_feet=-83,
+        y_feet=65,
+    ).asdict(),
+
+    # HallwayPointNode(
+    #     id="corner_rm005_2",
+    #     x_feet=-83,
+    #     y_feet=65,
+    # ).asdict(),
+
+    HallwayPointNode( #the turn from lab wing to the staircase area
+        id="corner_mid",
+        x_feet=-46,
+        y_feet=98,
+    ).asdict(),
+
+    HallwayPointNode(
+        id="corner_mid2",
+        x_feet=-46,
+        y_feet=94,
+    ).asdict(),
+
+    HallwayPointNode(
+        id="corner_mid3",
+        x_feet=-41,
+        y_feet=4.5,
+    ).asdict(),
+
+    HallwayPointNode(
+        id="corner_lobby_mid",
+        x_feet=-111,
+        y_feet=94,
+    ).asdict(),
+
+    HallwayPointNode(
+        id="corner_lobby_north",
+        x_feet=-111,
+        y_feet=145,
+    ).asdict(),
+
+    HallwayPointNode(
+        id="corner_lobby_south",
+        x_feet=-111,
+        y_feet=87,
+    ).asdict(),
+
+    HallwayPointNode(
+        id="corner_lobby_west",
+        x_feet=-121,
+        y_feet=87,
+    ).asdict(),
+    
 ]
 
 # NODES = [
